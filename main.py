f76657a92fd908f3ff27f768a3f4e9c491565103
liam-foulger <65193496+liam-foulger@users.noreply.github.com>
2025-07-14 10:15:31 -0700

    add fp_a offsets & update calibration file

--- main.py
+++ main.py
@@ -17,23 +17,34 @@ SHARED_DATA_PATH = os.path.join(Path(os.environ.get("OneDrive")), "EVSFR")
 
 data_path = os.path.join(SHARED_DATA_PATH, "pilotdata", "lf", "EVSFR_PilotLF_Trial2_ec_1ma_stim.txt")
 
-# read in force plate calibrations
-# // force plate a calibration data
+# read in force plate calibrations (SN: 5571)
+# // force plate a calibration data 
 fpa_cal = pd.read_csv("fp_cal/fpa.csv", header=None).to_numpy()
-# V0 = 10
-# gain = 4000
-# offsets = np.array([0.0004318, -0.0006096, -0.0413766])  # !! must be changed for fp_a
+offsets_a = np.array([0.001024, -0.000411, -0.042919])
 
-# // force plate b calibration data
+# // force plate b calibration data (SN: 8032)
 fpb_cal = pd.read_csv("fp_cal/fpb.csv", header=None).to_numpy()
+offsets_b = np.array([0.0004318, -0.0006096, -0.0413766])
+
 V0 = 10
 gain = 4000
-offsets = np.array([0.0004318, -0.0006096, -0.0413766])
 
 # read in experimental data
 headernames = pd.read_csv("resources/headernames.csv", sep=",", header=0).columns.tolist()
 df = pd.read_csv(data_path, sep=",", header=0, names=headernames)
 # %%
+fpa = forcePlate(
+    fx=df["fpa_fx"].to_numpy(),
+    fy=df["fpa_fy"].to_numpy(),
+    fz=df["fpa_fz"].to_numpy(),
+    mx=df["fpa_mx"].to_numpy(),
+    my=df["fpa_my"].to_numpy(),
+    mz=df["fpa_mz"].to_numpy(),
+    v0=V0,
+    gain=gain,
+    calmatrix=np.linalg.inv(fpa_cal),
+    offsets=offsets_a,
+)
 
 fpb = forcePlate(
     fx=df["fpb_fx"].to_numpy(),
@@ -45,7 +56,7 @@ fpb = forcePlate(
     v0=V0,
     gain=gain,
     calmatrix=np.linalg.inv(fpb_cal),
-    offsets=offsets,
+    offsets=offsets_b,
 )
 
 imu = IMU(
